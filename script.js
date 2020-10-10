25c42dbf13097aace750a413a782f3d683482ad0
Zach Smelcer <zacaronithesmelcer@icloud.com>
2020-10-10 16:18:51 -0500

    Made exponential changes to the functionality, broke it, and fixed it to perfection.

--- script.js
+++ script.js
@@ -12,13 +12,13 @@ function getBreweries(cityName) {
     method: "GET",
     headers: {
       Authorization:
-        "Bearer uYiXPDKaT7WGhv2IiyHMIspnpwCYhdMtmMx-evY72HPrl5Q2dYYK4IdGjOWz56SqTF-aaIit1Ke5qwHcX7lH-1wASlG4CVuTQhQhqD2uDUKKxbu_sctJlQsHgEB_X3Yx",
-    },
+        "Bearer ajiK8lEyrwTd3zrvz8EWzy8vgjLgQrSlaAGU0Mjdb0Ax_GFqJRJZoMIUKEJ99izPtUQb7qwDiJqAoUgICmuO3s4fV9Hvnzub6N1PdbOTaD7Nc18dyjW_d0nGBPmBX3Yx",
+    }
   };
 
-  console.log('about to do ajax call here our seetings', settings)
-  apiSmack(settings)
-}
+  console.log('about to do ajax call here our seetings', settings);
+  apiSmack(settings);
+};
 
 function getFood(cityName) {
   var settings = {
@@ -30,12 +30,12 @@ function getFood(cityName) {
     method: "GET",
     headers: {
       Authorization:
-        "Bearer uYiXPDKaT7WGhv2IiyHMIspnpwCYhdMtmMx-evY72HPrl5Q2dYYK4IdGjOWz56SqTF-aaIit1Ke5qwHcX7lH-1wASlG4CVuTQhQhqD2uDUKKxbu_sctJlQsHgEB_X3Yx",
+        "Bearer ajiK8lEyrwTd3zrvz8EWzy8vgjLgQrSlaAGU0Mjdb0Ax_GFqJRJZoMIUKEJ99izPtUQb7qwDiJqAoUgICmuO3s4fV9Hvnzub6N1PdbOTaD7Nc18dyjW_d0nGBPmBX3Yx",
     }
   };
 
   console.log('about to do ajax call here our seetings', settings)
-  apiSmack(settings)
+  apiSmack(settings);
 }
 
 function getWeather(cityName){
@@ -52,53 +52,37 @@ function getWeather(cityName){
 
 };
 
-function getReviews(cityId){
-  var settings = {
-      async: true,
-      crossDomain: true,
-      url:
-      "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/" + cityId + "/reviews",
-      method: "GET",
-      headers: {
-        Authorization:
-          "Bearer uYiXPDKaT7WGhv2IiyHMIspnpwCYhdMtmMx-evY72HPrl5Q2dYYK4IdGjOWz56SqTF-aaIit1Ke5qwHcX7lH-1wASlG4CVuTQhQhqD2uDUKKxbu_sctJlQsHgEB_X3Yx",
-      }
-    };
-    console.log(cityId);
-    apiSmack(settings);
-};
   
 
-// make on click for rebiew button
-  // do this in on click getReviews($(this).attr('name'))
-
 function apiSmack(settings) {
+  
   $.ajax(settings).done(function (response) {
     console.log(response);
-    
     var destName = $("#destName");
     var cityHeading = $("#destination");
-
+    
     if(response.businesses){
     cityHeading.text(response.businesses[0].location.city);
+    console.log("Im in the response.business if statement");
 
     for (var i = 0; i < 5; i++) {
       // 1 make a piece of html with jquery
+      console.log("IM in the for loop about to create content");
       var resterauntContainer = $("<div class = 'placeContainer'>");
       var resterauntTitle = $('<h4>')
 
       var imageDiv = $("<div class='imageDiv'>");
-      var resterauntImage = $('<img>')
-      var phone = $('<p>')
+      var resterauntImage = $('<img>');
+
+      var phone = $('<p>');
       var address = $('<p>');
 
       var detailContainer = $("<div class = 'detail-container'>");
       var price = $('<p>');
       var rating = $('<p>');
       var reviewCount = $('<p>');
-      var reviewButton = $('<button>');
       var getimgUrl = response.businesses[i].image_url;
-      var cityId = response.businesses[i].id;
+    console.log("Made div content in loop");
 
       //getReviews(cityId)
       //2 dress it up how u want classess text ect.
@@ -106,57 +90,57 @@ function apiSmack(settings) {
       resterauntTitle.text(response.businesses[i].name)
       resterauntImage.attr("src", getimgUrl);
       resterauntImage.addClass("images");
-      reviewButton.attr('name', cityId)
-      console.log("I got through after setting the image attribute");
       var addressDiv = $("<div class = 'address'>");
-      phone.text(response.businesses[i].display_phone)
-      phone.addClass('phone')
+      phone.text(response.businesses[i].display_phone);
+      phone.addClass('phone');
       // review.text(response)
-      resterauntContainer.addClass('searchResult-Container')
+      resterauntContainer.addClass('searchResult-Container');
       address.text("Address: " + response.businesses[i].location.display_address[0] +  " " + response.businesses[i].location.display_address[1]);
 
-      price.text("Expected Cost: " + response.businesses[i].price)
+      price.text("Expected Cost: " + response.businesses[i].price);
       rating.text("Rating: " + response.businesses[i].rating + " ⭐️");
-      reviewCount.text("Review Count: " + response.businesses[i].review_count)
-
+      reviewCount.text("Review Count: " + response.businesses[i].review_count);
+      console.log("dressed up the content in loop");
+      
       //3 .append that sucker to the page!!
       imageDiv.append(resterauntImage);
       detailContainer.append(price, rating, reviewCount);
       addressDiv.append(phone, address)
       resterauntContainer.append(resterauntTitle, detailContainer, imageDiv, addressDiv);
-      
-      $('#foodDiv').append(resterauntContainer)
+       $('#foodDiv').append(resterauntContainer);
+      console.log("appended created content in loop");
     }
     
     };
 
     if(response.list){
-    var weatherDiv = $("<div>");
+    
+    var weatherDiv = $("#weatherDiv");
     var weather = $("<p>");
     var dataTemp = Math.floor((response.list[0].main.temp - 273.15) * 1.80 + 32);
     weather.text("Temperature: " + dataTemp + " °F");
     weatherDiv.append(weather);
-    destName.append(cityHeading, weatherDiv);
+    destName.append(weatherDiv);
     };
-    
-    
-
-    
-  })
-}
+  
+  });
+};
 
 
 searchBTN.on("click", function (e) {
-  console.log('we got clciked!!!')
+  console.log('we got clciked!!!');
   e.preventDefault();
+  $('#foodDiv').html("");
+  $('#weatherDiv').html("");
+  $("#cityInfos").css("visibility", "visible");
   var cityName = searchInput.val();
   getBreweries(cityName)
   getFood(cityName)
   getWeather(cityName);
-
   
-
 });
+
+
 
   // var settings2 = {
   //   async: true,
